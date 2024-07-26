16ff232ea7fca6b23c6c37e78543a33e94adb51a
thy646742 <thy646742@163.com>
2024-07-26 16:13:15 +0800

    feat: add styled list that shows song information

--- src/AllSongsPage.tsx
+++ src/AllSongsPage.tsx
@@ -2,9 +2,9 @@ import { weapi } from './encrypt';
 import axios from 'axios';
 
 const AllSongsPage = () => {
-    const [ songList, setSongList ] = React.useState([]); 
-    const [ currentPage, setCurrentPage] = React.useState(1);
-    const [ pageLength, setPageLength ] = React.useState(1000);
+    const [ songList, setSongList ] = React.useState([]);
+    const [ currentPage, setCurrentPage ] = React.useState(1);
+    const [ pageLength, setPageLength ] = React.useState(10);
     const artistId = new URLSearchParams(window.location.href.split('?')[1]).get("id");
 
     const data = {
@@ -16,7 +16,7 @@ const AllSongsPage = () => {
         private_cloud: true,
         work_type: 1,
     };
-    
+
     React.useEffect(() => {
         const sendRequest = async () => {
             const encryptedData = weapi(data);
@@ -25,7 +25,7 @@ const AllSongsPage = () => {
                 url: "http://music.163.com/weapi/v1/artist/songs",
                 data: new URLSearchParams(encryptedData).toString()
             };
-            try{
+            try {
                 const response = await axios(requestConfig);
                 setSongList(response.data.songs);
             } catch (error) {
@@ -39,11 +39,103 @@ const AllSongsPage = () => {
         console.log(songList);
     });
 
-    return(
-        <ul>
-            {songList.map(song => (<li>{song.name}</li>))}
-        </ul>
+    // It seems like the data-res like attributes are for providing parameters for interactions
+    // But some of them are also detected for styling...
+
+    return (
+        <div className="m-plylist m-plylist-pl2 m-plylist_playlist m-plylist-sort"
+            tabIndex={1000} id="all-songs-list">
+            <div className="head sort f-cb j-flag">
+                <div className="fix">
+                    <div className="th col">
+                        <span title="还原默认排序" className="icn-sort-btn">
+                            <a className="j-flag"><i></i></a>
+                            <a className="f-dn z-def j-flag">
+                                <svg className="u-icn u-icn-sort-reset"></svg>
+                            </a>
+                        </span>
+                    </div>
+                    <div className="th col">
+                        操作
+                        <svg className="u-icn u-icn-sort-hvr"></svg>
+                        <svg className="u-icn u-icn-sort"></svg>
+                    </div>
+                </div>
+                <div className="flow j-flag">
+                    <div className="th col" data-res-action="sort" data-res-field="title">
+                        <span>标题 </span>
+                    </div>
+                    <div className="th col" data-res-action="sort" data-res-field="artist">
+                        <span>歌手</span>
+                    </div>
+                    <div className="th col" data-res-action="sort" data-res-field="album">
+                        <span>专辑</span>
+                    </div>
+                    <div className="th col" data-res-action="sort" data-res-field="duration">
+                        <span>时间</span>
+                    </div>
+                </div>
+            </div>
+            <ul className="j-flag">
+                <div id="all-songs-list-wrapper-1" tabIndex={10000}>
+                    <div className="lst fixed-scroll-management" id="all-songs-list-wrapper-2">
+                        <div className="pl-di pl-di-1">
+                            {
+                                songList.map(song => (
+                                    <li
+                                        className="itm j-item j-impress"
+                                        data-res-menu="true"
+                                    >
+                                        <span className="playstate u-micn u-plyst"></span>
+                                        <span
+                                            title="喜欢"
+                                            className="z-off ico z-first u-micn u-micn-love f-cp s-fc3"
+                                            data-res-action="like"
+                                        ></span>
+                                        <span
+                                            className="z-off ico z-dis"
+                                            data-res-action="download"
+                                        >
+                                            <span className="u-micn u-micn-dld f-cp s-fc3"></span>
+                                        </span>
+                                        <div className="flow">
+                                            <div className="td col title">
+                                                <span className="tit s-fc1" title={song.name}>
+                                                    {song.name}
+                                                </span>
+                                            </div>
+                                            <div className="td col ellipsis s-fc3 f-pr" title={song.ar.map(author => (author.name)).join(" / ")}>
+                                                {
+                                                    // This part was helped by chatGPT, I don't know why his works and mine doesn't...
+                                                    song.ar.map((author, index, array) => (
+                                                        <React.Fragment key={author.id}>
+                                                            <a className="s-fc3" href={"#/m/artist/?id=" + author.id}>
+                                                                {author.name}
+                                                            </a>
+                                                            {index < array.length - 1 && ' / '}
+                                                        </React.Fragment>
+                                                    ))
+                                                }
+                                            </div>
+                                            <div className="td col ellipsis">
+                                                <a className="s-fc3" href={"#/m/album/?id=" + song.al.id} title={song.al.name}>
+                                                    {song.al.name}
+                                                </a>
+                                            </div>
+                                            <div className="td col s-fc4">
+                                                {Math.floor(Math.floor(song.dt / 1000) / 60)}:{String(Math.floor(song.dt / 1000) % 60).padStart(2, '0')}
+                                            </div>
+                                        </div>
+                                    </li>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+            </ul>
+        </div>
     )
+
 }
 
 export { AllSongsPage };
